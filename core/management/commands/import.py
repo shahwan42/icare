b13b56b683c42f36700f87fd87a06e2c063089a0
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-12 05:43:31 +0200

    fix Command

--- core/management/commands/import.py
+++ core/management/commands/import.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 
 
-class Import(BaseCommand):
+class Command(BaseCommand):
     help = "Import Spaces, Folders, Lists from ClickUp"
 
     def handle(self, *args, **kwargs):
         # Import Spaces
         # Import Folders
         # Import Lists
-        pass
+        self.stdout.write("Handling Imports...")
