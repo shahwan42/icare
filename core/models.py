a3924dd1a0ade5b2a0db4b759bedd11c4b171026
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-12 10:25:34 +0200

    Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CEntity(models.Model):
+    """ClickUp entity base model"""
+
+    c_id = models.PositiveIntegerField(_("ClickUp ID"), unique=True)
+    is_active = models.BooleanField(_("is active?"))
+    name = models.TextField(_("name"))
+    description = models.TextField(_("description"))
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<{__class__.__name__}, id: {self.c_id}>"
+
+
+class CSpace(CEntity):
+    """ClickUp Space representation"""
+
+
+class CFolder(CEntity):
+    """ClickUp Folder representation"""
+
+
+class CList(CEntity):
+    """ClickUp List representation"""
+
+
+class CTask(CEntity):
+    """ClickUp Task representation"""
+
+    c_id = models.CharField(_("ClickUp ID"), max_length=12, unique=True)
