ba496b262091310acd1e437964a6ca3955c867f4
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-13 23:36:31 +0200

    migrate to postgresql

--- ctb/settings.py
+++ ctb/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = "ctb.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env("POSTGRES_URL"),
+        "PORT": env("POSTGRES_PORT"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
     }
 }
 
