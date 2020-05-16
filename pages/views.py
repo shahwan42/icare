15f3ed935534d5ce31aab2a040a335d84dc38769
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-16 03:37:27 +0200

    list folders in home instead of spaces

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,18 @@
 from django.views.generic import ListView
 
-from core.models import CSpace
+from core.models import CSpace, CFolder
+
+
+class HomeWithSpaces(ListView):
+    """DEPRECATED
+    List active spaces to put tasks in"""
+
+    template_name = "pages/home.html"
+    queryset = CSpace.objects.filter(is_active=True)
 
 
 class Home(ListView):
+    """List active folders to put tasks in"""
+
     template_name = "pages/home.html"
-    queryset = CSpace.objects.filter(is_active=True)
+    queryset = CFolder.objects.filter(is_active=True)
