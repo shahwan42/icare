b1c0d271df473b68f95367a7ff7a5c024d2e8863
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-16 09:01:18 +0200

    try again whitenoise

--- ctb/settings.py
+++ ctb/settings.py
@@ -159,6 +159,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 AUTH_USER_MODEL = "users.CustomUser"
