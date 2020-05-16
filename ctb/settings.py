82889aa656679894461bbbe5833035cb9e839b5b
Ahmed Shahwan <ahmed@shahwan.me>
2020-05-16 08:52:39 +0200

    no static dirs for now

--- ctb/settings.py
+++ ctb/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
