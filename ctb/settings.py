306d41ecb71b754780ddfc3b66b85a9e5005c43d
Ahmed Shahwan <ahmed@shahwan.me>
2020-06-13 14:58:01 +0200

    static_root

--- ctb/settings.py
+++ ctb/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
