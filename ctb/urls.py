66e745b3f6f589e8f225be3dece76e0cd534e5b1
Ahmed Shahwan <ahmed@shahwan.me>
2020-06-13 13:53:28 +0000

    removing whitenoise

--- ctb/urls.py
+++ ctb/urls.py
@@ -8,8 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home') Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
