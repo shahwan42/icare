ca63b86422d764909f6d6d55e1a40f72e6b0afb8
Ahmed Shahwan <ahmed@shahwan.me>
2020-06-13 16:00:39 +0200

    remove uwsgi stuff

--- ctb/wsgi.py
+++ ctb/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append("/home/ctb/public_html/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctb.settings")
 
 application = get_wsgi_application()
