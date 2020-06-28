4c2e587b6c8bad939fae78d12e43249617048df5
Ahmed Shahwan <ahmed@shahwan.me>
2020-06-28 10:00:53 +0200

    update deploy files

--- deploy/gunicorn_config.py
+++ deploy/gunicorn_config.py
@@ -1,5 +1,5 @@
 command = "/home/as/.virtualenvs/icare/bin/gunicorn"
 pythonpath = "/home/as/.virtualenvs/icare/bin/python"
-bind = "127.0.0.1:8002"
+bind = "127.0.0.1:8003"
 workers = 3
 user = "as"
