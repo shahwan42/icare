a19067913d561704739012466bf12a555b6c44e6
Ahmed Shahwan <ahmed@shahwan.me>
2020-07-05 04:42:38 +0200

    disable ordinary views, update requirements

--- project/urls.py
+++ project/urls.py
@@ -1,17 +1,3 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home') Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import Group
@@ -62,24 +48,24 @@ api_urls = [
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", Home.as_view(), name="home"),
-    path("task_updated", TaskUpdatedWebhook.as_view(), name="task_updated"),
-    path("new_task/<int:folder_id>", NewTask.as_view(), name="new_task"),
-    path(
-        "lists/<int:pk>/custom_fields",
-        ListCustomFields.as_view(),
-        name="list_custom_fields",
-    ),
-    path(
-        "new_task/success",
-        TemplateView.as_view(template_name="core/new_task_success.html"),
-        name="new_task_success",
-    ),
+    # path("", Home.as_view(), name="home"),
+    # path("task_updated", TaskUpdatedWebhook.as_view(), name="task_updated"),
+    # path("new_task/<int:folder_id>", NewTask.as_view(), name="new_task"),
+    # path(
+    #     "lists/<int:pk>/custom_fields",
+    #     ListCustomFields.as_view(),
+    #     name="list_custom_fields",
+    # ),
+    # path(
+    #     "new_task/success",
+    #     TemplateView.as_view(template_name="core/new_task_success.html"),
+    #     name="new_task_success",
+    # ),
     # path("users/", include("users.urls")),
-    path("users/tasks", UserTasks.as_view(), name="user_tasks"),
-    path("users/profile/<int:pk>", Profile.as_view(), name="user_profile"),
-    path("users/password_change/", ChangePassword.as_view(), name="password_change"),
-    path("users/", include("django.contrib.auth.urls")),
+    # path("users/tasks", UserTasks.as_view(), name="user_tasks"),
+    # path("users/profile/<int:pk>", Profile.as_view(), name="user_profile"),
+    # path("users/password_change/", ChangePassword.as_view(), name="password_change"),
+    # path("users/", include("django.contrib.auth.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ] + api_urls
 
