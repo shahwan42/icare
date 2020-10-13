660ad168266f3951bf24ea4558644bc97f907c90
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-13 23:01:53 +0200

    update gunicorn config

--- deploy/gunicorn_config.py
+++ deploy/gunicorn_config.py
@@ -1,3 +1,5 @@
+# copy to ./local_gunicorn_config.py
+
 command = "/home/USERNAME/.virtualenvs/VENV_NAME/bin/gunicorn"
 pythonpath = "/home/USERNAME/.virtualenvs/VENV_NAME/bin/python"
 bind = "127.0.0.1:8003"
