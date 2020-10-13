2a9ecabeff0060dac58e79b2686585b45d101d68
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-13 22:48:20 +0200

    update deploy files to be generic

--- deploy/gunicorn_config.py
+++ deploy/gunicorn_config.py
@@ -1,5 +1,5 @@
-command = "/home/as/.virtualenvs/icare/bin/gunicorn"
-pythonpath = "/home/as/.virtualenvs/icare/bin/python"
+command = "/home/USERNAME/.virtualenvs/VENV_NAME/bin/gunicorn"
+pythonpath = "/home/USERNAME/.virtualenvs/VENV_NAME/bin/python"
 bind = "127.0.0.1:8003"
 workers = 3
-user = "as"
+user = "USERNAME"
